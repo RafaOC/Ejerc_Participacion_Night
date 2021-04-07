aa2fdb76194ba8292515db58e85a73e7394b5171
RafaOC <78857507+RafaOC@users.noreply.github.com>
2021-04-06 22:17:17 -0400

    Add files via upload

---
+++ 1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Ejerc_Night_3
+{
+    // 1 ---> Crear un metodo que retorne 5 valores random entre 1 y 30 mediante parametros por referencia.
+class Program
+    {
+        public void CincoRandoms(out int num1, out int num2, out int num3, out int num4, out int num5)
+        {
+            Random r = new Random();
+            num1 = r.Next(1, 30);
+            num2 = r.Next(1, 30);
+            num3 = r.Next(1, 30);
+            num4 = r.Next(1, 30);
+            num5 = r.Next(1, 30);            
+        }
+         static void Main(string[] args)
+        {
+            int random1, random2, random3, random4, random5;
+
+            Program p = new Program();
+            p.CincoRandoms(out random1, out random2, out random3, out random4, out random5);
+            
+            Console.WriteLine("Primer valor aleatorio:" + random1);
+            Console.WriteLine("Segundo valor aleatorio:" + random2);
+            Console.WriteLine("Tercer valor aleatorio:" + random3);
+            Console.WriteLine("Cuarto valor aleatorio:" + random4);
+            Console.WriteLine("Quinto valor aleatorio:" + random5);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 2.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Ejerc_Night_4
+{
+    /* 2 --- > Crear un programa que permita crear, cargar y obtener el menor y mayor valor de un vector, 
+    La obtención del mayor y menor hacerlo en un único método que retorne dichos dos valores */
+
+    class Program
+    {
+        private int[] array;
+ 
+        public Program()
+        {
+            Console.Write("\n ¿De que dimension desea que sea el array?  \n");
+            int dimension = int.Parse(Console.ReadLine());
+            array = new int[dimension];
+            Console.Clear();
+        }
+ 
+        public void Cargar()
+        {
+            for (var i = 0; i < array.Length; i++)
+            {
+                Console.Write("\n Ahora llena el array con numeros: \n");
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            Console.Clear();
+        }
+ 
+        public void ObtenMayMe(out int numMa,out int numMe)
+        {
+            numMa=array[0];
+            numMe=array[0];
+            for (var i = 1; i < array.Length; i++)
+            {
+                if (array[i] > numMa)
+                {
+                    numMa = array[i];
+                }
+                else
+                {
+                    if (array[i] < numMe)
+                    {
+                        numMe = array[i];
+                    }
+                }
+            }
+        }
+ 
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.Cargar();
+            int mayor, menor;
+            p.ObtenMayMe(out mayor, out menor);
+            Console.WriteLine("\n El Mayor del array es {0} y El Menor es {1}.",mayor, menor);
+            Console.WriteLine("\n Presiona una tecla para finalizar...");
+            Console.ReadKey();
+        }
+    }
+}
